d9dd677b58bccaf57b083abaa5f84757c6ef13d8
Çağrı <cagcak@outlook.com>
2019-10-17 13:11:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-const getProductInfo = (productUrl = 'https://www.trendyol.com/farmasi/bb-krem-all-in-one-orta-50ml-8690131773984-p-4360856?boutiqueId=347542&merchantId=115290') => {
+const getProductInfo = async (productUrl = 'https://www.trendyol.com/farmasi/bb-krem-all-in-one-orta-50ml-8690131773984-p-4360856?boutiqueId=347542&merchantId=115290') => {
     const   axios = require('axios'),
             jsdom = require('jsdom'),
             { JSDOM } =  jsdom,
             url = productUrl;
+    try {
+        const res = await axios.get(url)
+        .then( response => response.data )
+        .then( html => {
+            const dom = new JSDOM(html);
+            query1 = dom.window.document.querySelectorAll('.pr-in-w .pr-in-br a');
+            brand = query1[0].innerHTML;
+            query2 = dom.window.document.querySelectorAll('.pr-in-w .pr-in-br span');
+            title = query2[0].innerHTML;
+            query3 = dom.window.document.querySelectorAll('.ph-gl-slc .slick-slider .slick-track');
+            for( let i=0; i<query3[0].children.length; i++ ) {
+                if( query3[0].children[i].getAttribute('data-index') == 0)
+                    var picture = query3[0].children[i].children[0].children[0].children[0].src;
+            }
+            query4 = dom.window.document.querySelectorAll('.pr-in-w .pr-bx-w .prc-slg');
+            price = query4[0].innerHTML;
+            const data = {
+                brand: brand,
+                title: title,
+                picture: picture,
+                price: price
+            };
+            
+            return data;
+        })
+        return res;
+    } catch (error) {
+        console.error(error)
+    }
 
-    axios.get(url)
-    .then( response => response.data )
-    .then( html => {
-        const dom = new JSDOM(html);
-        query1 = dom.window.document.querySelectorAll('.pr-in-w .pr-in-br a');
-        brand = query1[0].innerHTML;
-        query2 = dom.window.document.querySelectorAll('.pr-in-w .pr-in-br span');
-        title = query2[0].innerHTML;
-        query3 = dom.window.document.querySelectorAll('.ph-gl-slc .slick-slider .slick-track');
-        for( let i=0; i<query3[0].children.length; i++ ) {
-            if( query3[0].children[i].getAttribute('data-index') == 0)
-                var picture = query3[0].children[i].children[0].children[0].children[0].src;
-        }
-        query4 = dom.window.document.querySelectorAll('.pr-in-w .pr-bx-w .prc-slg');
-        price = query4[0].innerHTML;
-        const data = {
-            brand: brand,
-            title: title,
-            picture: picture,
-            price: price
-        };
-        
-        return data;
-    })
 }
 
-console.log( getProductInfo() )
+getProductInfo().then(a => {
+    console.log(getProductInfo())
+})
