71a1ac8a3af2a54894d9acc8ffaeab4972f70570
Şems Yılmaz <akgulmuhammed@live.com>
2019-10-31 19:26:56 +0300

    module export added and its ready for npm

--- index.js
+++ index.js
@@ -1,33 +1,61 @@
-const getProductInfo = (productUrl = 'https://www.trendyol.com/farmasi/bb-krem-all-in-one-orta-50ml-8690131773984-p-4360856?boutiqueId=347542&merchantId=115290') => {
-    const   axios = require('axios'),
-            jsdom = require('jsdom'),
-            { JSDOM } =  jsdom,
-            url = productUrl;
-
-    axios.get(url)
-    .then( response => response.data )
-    .then( html => {
-        const dom = new JSDOM(html);
-        query1 = dom.window.document.querySelectorAll('.pr-in-w .pr-in-br a');
-        brand = query1[0].innerHTML;
-        query2 = dom.window.document.querySelectorAll('.pr-in-w .pr-in-br span');
-        title = query2[0].innerHTML;
-        query3 = dom.window.document.querySelectorAll('.ph-gl-slc .slick-slider .slick-track');
-        for( let i=0; i<query3[0].children.length; i++ ) {
-            if( query3[0].children[i].getAttribute('data-index') == 0)
-                var picture = query3[0].children[i].children[0].children[0].children[0].src;
-        }
-        query4 = dom.window.document.querySelectorAll('.pr-in-w .pr-bx-w .prc-slg');
-        price = query4[0].innerHTML;
-        const data = {
-            brand: brand,
-            title: title,
-            picture: picture,
-            price: price
-        };
-        
-        return data;
-    })
+function divideForTrendyol(dom) {
+    const query1 = dom.window.document.querySelectorAll('.pr-in-w .pr-in-br a'),
+          brand = query1[0].text,
+          query2 = dom.window.document.querySelectorAll('.pr-in-w .pr-in-br span'),
+          title = query2[0].innerHTML,
+          query3 = dom.window.document.querySelectorAll('.ph-gl-img'),
+          picture = query3[0].src,
+          query4 = dom.window.document.querySelectorAll('.pr-bx-pr-dsc .prc-dsc').length !== 0 ? dom.window.document.querySelectorAll('.pr-bx-pr-dsc .prc-dsc') : dom.window.document.querySelectorAll('.pr-in-w .pr-bx-w .prc-slg'),
+          price = query4[0].innerHTML;
+    const product = { 'brand': brand, 'title': title, 'picture': picture, 'price': price};
+    return product;
 }
 
-console.log( getProductInfo() )
+function divideForHepsiburada(dom) {
+    const query1 = dom.window.document.querySelectorAll('.productDetailContent .brand-name a'),
+          brand = query1[0].innerHTML.trim(),
+          query2 = dom.window.document.querySelectorAll('#product-name'),
+          title = query2[0].innerHTML.trim(),
+          query3 = dom.window.document.querySelectorAll('#offering-price'),
+          price = query3[0].getAttribute('content'),
+          query4 = dom.window.document.querySelectorAll('#image-0.product-image')[0].src,
+          picture = query4;
+    const product = { 'brand': brand, 'title': title, 'picture': picture, 'price': price + ' TL'};
+    return product;
+}
+
+function getData(url) {
+    const axios = require('axios');
+    return new Promise(resolve => {
+        axios.get(url)
+        .then(response => resolve(response.data))
+    });
+}
+
+module.exports = async function getProductInfo(productUrl) {
+    const   jsdom = require('jsdom'),
+            { JSDOM } =  jsdom;
+    const html = await getData(productUrl);
+    const dom = new JSDOM(html);
+    
+    try {
+    const urlAddress = new URL(productUrl);
+    let product;
+
+    switch(urlAddress.origin) {
+        case 'https://www.trendyol.com':
+            product = divideForTrendyol(dom);
+            break;
+        case 'https://www.hepsiburada.com':
+            product = divideForHepsiburada(dom);
+            break;
+        default:
+            break;
+    }
+
+    return product;
+    } catch(e) {
+        console.error(e)
+    };
+
+}
